ada2d6d9ea4c51a1cf2975a2d30457e0f15c5b9a
Thomas Urwin <thomas.urwin@hotmail.com>
2017-04-30 21:42:09 +1000

    update repo with final prototype

--- main.lua
+++ main.lua
@@ -19,9 +19,9 @@ background.anchorX, background.anchorY =0, 0.5
 background.x, background.y = 0, display.contentHeight/2
 background:scale(display.contentHeight/background.height, display.contentHeight/background.height)
 
-local myMap = native.newMapView(0,0, display.contentWidth*0.9, display.contentHeight*0.5)
+local myMap = native.newMapView(0,0, display.contentWidth, display.contentHeight*0.73)
 myMap.x = display.contentCenterX
-myMap.y = display.contentHeight*0.3
+myMap.y = (display.contentHeight*0.73) / 2
 myMap.mapType = "standard"
 
 local saveLocButtonPress = function( event )
@@ -38,13 +38,13 @@ local saveLocButtonPress = function( event )
         if not file then
             -- Error occurred; output the cause
             print( "File error: " .. errorString )
-            native.showAlert("Error", "File not found.")
+            --native.showAlert("Error", "File not found.")
         else
             -- Write data to file
             file:write( saveData )
             -- Close the file handle
             io.close( file )
-            native.showAlert("Success", "Location Saved: "..path.."   DATA: "..locationData.latitude..", "..locationData.longitude)
+            --native.showAlert("Success", "Location Saved: "..path.."   DATA: "..locationData.latitude..", "..locationData.longitude)
             viewingCar = false
             myMap:removeAllMarkers()
         end
@@ -57,26 +57,30 @@ end
 
 local button1 = widget.newButton(
 {
-	defaultFile = "assets/button_1.png",
-	overFile = "assets/button_1_over.png",
-	label = "Save My Car's Location",
-	labelColor = 
-	{ 
-		default = { 0/255, 0/255, 0/255, 1}, 
-		over = { 0/255, 0/255, 0/255, 0/255 }
-	},
-	font = native.systemFontBold,
-	fontSize = 22,
-	emboss = true,
-	onPress = saveLocButtonPress,
+    shape = 'rect',
+    label = "Save My Car's Location",
+    labelColor = 
+    { 
+        default = { 0/255, 0/255, 0/255, 1}, 
+        over = { 0/255, 0/255, 0/255, 0/255 }
+    },
+    fillColor = { default={0,1,0,1}, over={0.3,1,0.3,1} },
+    strokeColor = { default={1,1,1,1}, over={0.8,0.8,1,1} },
+    strokeWidth = 3,
+    font = native.systemFontBold,
+    fontSize = 22,
+    emboss = true,
+    width = display.contentWidth,
+    height = display.contentHeight * 0.13,
+    onPress = saveLocButtonPress,
 })
-button1.x, button1.y = display.contentWidth/2, 7*(display.contentHeight/10)
-button1:scale((display.contentWidth/button1.contentWidth)*0.9, (display.contentWidth/button1.contentWidth)*0.9)
+button1.x, button1.y = display.contentWidth/2, display.contentHeight - button1.contentHeight / 2
+--button1:scale((display.contentWidth/button1.contentWidth)*0.9, (display.contentWidth/button1.contentWidth)*0.9)
 
 
 
 local showLocButtonPress = function( event )
-	-- Show location on map
+    -- Show location on map
     local lat, lon, alt, acc
 
     local path = system.pathForFile( "parK_location.txt", system.DocumentsDirectory )
@@ -84,12 +88,12 @@ local showLocButtonPress = function( event )
     if not file then
         -- Error occurred; output the cause
         print( "File error: " .. errorString )
-        native.showAlert("Error", "File not found.")
+        --native.showAlert("Error", "File not found.")
     else
-        lat = tonumber(file:read("*l"))
-        lon = tonumber(file:read("*l"))
-        alt = tonumber(file:read("*l"))
-        acc = tonumber(file:read("*l"))
+        lat = tonumber(file:read("*l")); print("lat: " .. lat)
+        lon = tonumber(file:read("*l")); print("lon: " .. lon)
+        alt = tonumber(file:read("*l")); print("alt: " .. alt)
+        acc = tonumber(file:read("*l")); print("acc: " .. acc)
 
         -- Close the file handle
         io.close( file )
@@ -98,44 +102,48 @@ local showLocButtonPress = function( event )
     file = nil
 
     if lon ~= 0 then
-	    --mapURL = "https://maps.google.com/maps?q=Your,+Car@" .. locationData.latitude .. "," .. locationData.longitude 
-	    --system.openURL( mapURL )
+        mapURL = "https://maps.google.com/maps?q=Your,+Car@" .. locationData.latitude .. "," .. locationData.longitude 
+        system.openURL( mapURL )
         viewingCar = true
-        native.showAlert("Location", lat..", "..lon..","..alt..", "..acc)
+        --native.showAlert("Location", lat..", "..lon..","..alt..", "..acc)
         myMap:setCenter(lat, lon)
         myMap:addMarker(lat, lon, "assets/car_icon.png")
     else
-        native.showAlert("Error", "locationData.longitude == 0")
+        --native.showAlert("Error", "locationData.longitude == 0")
     end
 end
 
 local button2 = widget.newButton
 {
-	defaultFile = "assets/button_2.png",
-	overFile = "assets/button_2_over.png",
-	label = "Find My Car!",
-	labelColor = 
-	{ 
-		default = { 0/255, 0/255, 0/255, 1}, 
-		over = { 0/255, 0/255, 0/255, 0/255 } 
-	},
-	font = native.systemFontBold,
-	fontSize = 22,
-	emboss = true,
-	onPress = showLocButtonPress,
+    shape = 'rect',
+    label = "Find My Car!",
+    labelColor = 
+    { 
+        default = { 0/255, 0/255, 0/255, 1}, 
+        over = { 0/255, 0/255, 0/255, 0/255 } 
+    },
+    fillColor = { default={0,0.4,1,1}, over={0.4,0,1,1} },
+    strokeColor = { default={1,1,1,1}, over={0.8,0.8,1,1} },
+    strokeWidth = 3,
+    font = native.systemFontBold,
+    fontSize = 22,
+    width = display.contentWidth,
+    height = display.contentHeight * 0.13,
+    emboss = true,
+    onPress = showLocButtonPress,
 }
-button2.x, button2.y = display.contentWidth/2, 9*(display.contentHeight/10)
-button2:scale((display.contentWidth/button2.contentWidth)*0.9, (display.contentWidth/button2.contentWidth)*0.9)
+button2.x, button2.y = display.contentWidth/2, display.contentHeight - button1.contentHeight - (button2.contentHeight / 1.95)
+--button2:scale((display.contentWidth/button2.contentWidth)*0.9, (display.contentWidth/button2.contentWidth)*0.9)
 
 
 
 local locationHandler = function( event )
     --native.showAlert("Success", "Location data received: "..event.latitude..", "..event.longitude.."  \\")
-	-- Check for error (user may have turned off Location Services)
-	if event.errorCode then
-		native.showAlert( "GPS Location Error", event.errorMessage, {"OK"} )
-		print( "Location error: " .. tostring( event.errorMessage ) )
-	else
+    -- Check for error (user may have turned off Location Services)
+    if event.errorCode then
+        --native.showAlert( "GPS Location Error", event.errorMessage, {"OK"} )
+        print( "Location error: " .. tostring( event.errorMessage ) )
+    else
     
         locationData.latitude = event.latitude
 
@@ -150,30 +158,30 @@ local locationHandler = function( event )
         end
 
         --[[locationData.latitude = event.accuracy
-		local latitudeText = string.format( '%.4f', event.latitude )
-		currentLatitude = latitudeText
-		latitude.text = latitudeText
-		
-		local longitudeText = string.format( '%.4f', event.longitude )
-		currentLongitude = longitudeText
-		longitude.text = longitudeText
-		
-		local altitudeText = string.format( '%.3f', event.altitude )
-		altitude.text = altitudeText
-	
-		local accuracyText = string.format( '%.3f', event.accuracy )
-		accuracy.text = accuracyText
-		
-		local speedText = string.format( '%.3f', event.speed )
-		speed.text = speedText
-	
-		local directionText = string.format( '%.3f', event.direction )
-		direction.text = directionText
-	
-		-- Note: event.time is a Unix-style timestamp, expressed in seconds since Jan. 1, 1970
-		local timeText = string.format( '%.0f', event.time )
-		time.text = timeText ]]
-		
-	end
+        local latitudeText = string.format( '%.4f', event.latitude )
+        currentLatitude = latitudeText
+        latitude.text = latitudeText
+        
+        local longitudeText = string.format( '%.4f', event.longitude )
+        currentLongitude = longitudeText
+        longitude.text = longitudeText
+        
+        local altitudeText = string.format( '%.3f', event.altitude )
+        altitude.text = altitudeText
+    
+        local accuracyText = string.format( '%.3f', event.accuracy )
+        accuracy.text = accuracyText
+        
+        local speedText = string.format( '%.3f', event.speed )
+        speed.text = speedText
+    
+        local directionText = string.format( '%.3f', event.direction )
+        direction.text = directionText
+    
+        -- Note: event.time is a Unix-style timestamp, expressed in seconds since Jan. 1, 1970
+        local timeText = string.format( '%.0f', event.time )
+        time.text = timeText ]]
+        
+    end
 end
 Runtime:addEventListener( "location", locationHandler )
